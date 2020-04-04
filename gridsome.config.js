d764a68c6432e838bf756e888c64fd934cbd6416
Jan Prill <Jan.Prill@mspag.com>
2020-04-04 12:28:19 +0200

    added tailwind starter kit

--- gridsome.config.js
+++ gridsome.config.js
@@ -47,5 +47,18 @@ module.exports = {
         publicPath: `/admin`
       }
     },
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      /* These are the default options. You don't need to set any options to get going.
+      options: {
+        tailwindConfig: './some/file/js',
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true
+      }
+      */
+    },
   ]
 }
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="layout">
+
     <header class="header">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
@@ -9,7 +10,11 @@
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
-    <slot/>
+
+    <main>
+      <slot/>
+    </main>
+
   </div>
 </template>
 
@@ -21,30 +26,3 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
