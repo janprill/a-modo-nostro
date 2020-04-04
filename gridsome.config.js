209014f7bb6b8453e4525ea99c795507e4987225
Jan Prill <Jan.Prill@mspag.com>
2020-04-04 10:08:29 +0200

    netlify cms

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,47 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'a modo nostro',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },    
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'pizza/**/*.md',
+        typeName: 'Pizza',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+  ]
 }
